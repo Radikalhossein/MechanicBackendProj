12331667e8373a0b3568d7e66d3376bed6241dd1
radikalhossein <hossein.tohi1381@gmail.com>
2024-01-26 16:59:05 +0330

    allows host

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ from models.users import User
 from routers.admin import router as admin_router
 from routers.service import router as service_router
 from routers.users import router as users_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 Base.metadata.create_all(engine)
 app.include_router(users_router, prefix="/user")
 app.include_router(admin_router, prefix="/admin")
